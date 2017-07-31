9d4f479e1f155e336ba678afd0b7761c2c60a5ec
Steve Yu <soundedrain@gmail.com>
2017-08-01 00:39:52 +0800

    [modify] adjust breadcrumb

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -14,6 +14,14 @@
           </a>
         </li>
         <li class="rt-breadcrumb-node">
+          <a class="rt-breadcrumb-link" data-level="2" data-type="breadcrumb-link" href="#"> <span class="rt-breadcrumb-link-text">不動產、百業、服務</span>  <i class="rti rti-angle-right-breadcrumb  rti-angle-right-subText"></i>
+          </a>
+        </li>
+        <li class="rt-breadcrumb-node">
+          <a class="rt-breadcrumb-link" data-level="2" data-type="breadcrumb-link" href="#"> <span class="rt-breadcrumb-link-text">不動產、百業、服務</span>  <i class="rti rti-angle-right-breadcrumb  rti-angle-right-subText"></i>
+          </a>
+        </li>
+        <li class="rt-breadcrumb-node">
           <a class="rt-breadcrumb-link" data-level="3" data-type="breadcrumb-link" href="#"> <span class="rt-breadcrumb-link-text">Ruten測試類別</span>
           </a>
         </li>
