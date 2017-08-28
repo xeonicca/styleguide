1799806a514913ec3cdaa6b6feb39e7b1ff7182b
Steve Yu <soundedrain@gmail.com>
2017-08-28 18:35:36 +0800

    [modify] adjst header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,20 +11,19 @@
     </div>
     <form id="srch" class="rt-header-form nav-search-form" action="" method="get" data-module="rtm-search">
       <input type="hidden" name="searchfrom" value="indexbar">
-      <div class="header-search-input-wrap nav-search-input">
+      <div class="header-search-wrap nav-search-input">
         <input id="keyword" class="header-search-input" name="k" type="search" placeholder="找商品" value="" size="1" />
-        <a href="#" class="header-search-clear rti-remove-defaultGrey" data-type="clear-key"><b>&times;</b></a>
+        <!-- <a href="#" class="header-search-clear rti-remove-defaultGrey" data-type="clear-key"><b>&times;</b></a> -->
       </div>
-      <div class="header-search-input-mask"></div>
+      <div class="header-search-mask" @click="expandMask"></div>
       <!-- <button class="header-search-button header-full-search-button nav-search-button nav-search-button-narrow js-all-key-search rti rti-search-decoWhite" type="submit" data-type="search-all">
       </button> -->
+
     </form>
     <a class="rt-header-cart" href="#"><span class="rt-header-counter">99</span></a>
     <a class="rt-header-chat" href="#"></a>
   </div>
-  <div class="rt-search">
-
-  </div>
+  <rt-search></rt-search>
 </div>
 
 </template>
@@ -35,6 +34,17 @@ export default {
   name: 'rt-header',
   components: {
     'rt-search': headerSearch
+  },
+  methods: {
+    expandMask() {
+      // if(!this.$el.classList.contains('rt-header-searchmode')) {
+      //   this.$el.classList.toggle('rt-header-searchmode')
+      // }
+
+      if(!document.body.classList.contains('search-mode')) {
+        document.body.classList.toggle('search-mode')
+      }
+    }
   }
 }
 </script>
--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -1,9 +1,38 @@
 <template>
   <div class="rt-search">
+    <div class="rt-search-header">
+      <a class="rt-search-prev" href="#"></a>
+      <form id="srch" class="rt-header-form nav-search-form" action="" method="get" data-module="rtm-search">
+        <input type="hidden" name="searchfrom" value="indexbar">
+        <div class="header-search-wrap nav-search-input">
+          <input id="keyword" class="header-search-input" name="k" type="search" placeholder="找商品" value="" size="1" />
+          <!-- <a href="#" class="header-search-clear rti-remove-defaultGrey" data-type="clear-key"><b>&times;</b></a> -->
+        </div>
+        <button class="header-search-button header-full-search-button" type="submit" data-type="search-all">
+        </button>
 
+      </form>
+    </div>
+    <div class="rt-search-body">
+      <div class="rt-section">
+        <h4 class="rt-section-title">歷史搜尋</h4>
+        <div class="rt-section-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio eligendi a voluptas impedit, mollitia reprehenderit recusandae velit fuga officia atque possimus doloribus quod in officiis ipsa laborum est aliquam nostrum!
+        </div>
+      </div>
+      <hr>
+      <div class="rt-section">
+        <h4 class="rt-section-title">熱門搜尋</h4>
+        <div class="rt-section-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio eligendi a voluptas impedit, mollitia reprehenderit recusandae velit fuga officia atque possimus doloribus quod in officiis ipsa laborum est aliquam nostrum!
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'rt-search',
+}
 </script>
