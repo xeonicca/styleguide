589a640af95e4d0cece4274007f235e6d44952e8
Steve Yu <soundedrain@gmail.com>
2017-08-29 18:48:48 +0800

    [modify] adjust keyword page

--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -9,8 +9,8 @@
             <option value="g">商品</option>
             <option value="s">賣家</option>
           </select>
-          <input id="keyword" class="header-search-input" name="k" type="search" placeholder="請輸入關鍵字" value="" size="1" />
-          <!-- <a href="#" class="header-search-clear rti-remove-defaultGrey" data-type="clear-key"><b>&times;</b></a> -->
+          <input id="keyword" class="header-search-input" name="k" type="search" placeholder="請輸入關鍵字" value="" size="1" v-model="keyword" />
+          <a href="#" class="header-search-clear" data-type="clear-key"><b>&times;</b></a>
         </div>
         <button class="header-search-button header-full-search-button" type="submit" data-type="search-all">
         </button>
@@ -21,14 +21,14 @@
       <div class="rt-section">
         <h4 class="rt-section-title">歷史搜尋</h4>
         <div class="rt-section-body">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio eligendi a voluptas impedit, mollitia reprehenderit recusandae velit fuga officia atque possimus doloribus quod in officiis ipsa laborum est aliquam nostrum!
+          <span v-for="word in historyKeywords" class="rt-search-keyword" @click="applyKeyword(word)">{{word}}</span>
         </div>
       </div>
       <hr>
       <div class="rt-section">
         <h4 class="rt-section-title">熱門搜尋</h4>
         <div class="rt-section-body">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio eligendi a voluptas impedit, mollitia reprehenderit recusandae velit fuga officia atque possimus doloribus quod in officiis ipsa laborum est aliquam nostrum!
+          <span v-for="word in hotKeywords" class="rt-search-keyword" @click="applyKeyword(word)">{{word}}</span>
         </div>
       </div>
     </div>
@@ -41,6 +41,32 @@ export default {
   methods: {
     closeSearch() {
       document.body.classList.remove('search-mode')
+    },
+
+    applyKeyword(word) {
+      this.keyword = word
+    }
+  },
+  props: ['searchKeyword'],
+  data() {
+    return {
+      keyword: this.searchKeyword,
+      historyKeywords: [
+        '威力彩',
+        '張小燕賣豐華',
+        '王孫辭世',
+        '鄒兆龍劈腿',
+        '早鳥機票'
+      ],
+
+      hotKeywords: [
+        '碎菜器',
+        '冰爆包',
+        '仿真抱枕',
+        '迷你裙',
+        '海賊王',
+        '夏期補習'
+      ]
     }
   }
 }
