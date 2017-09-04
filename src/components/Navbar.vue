d6d65f36aef9e59f08ab46c90b462bab01875ff9
Steve Yu <soundedrain@gmail.com>
2017-09-05 01:20:02 +0800

    [add] changed fixed footer to navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+<nav class="rt-navbar">
+  <div class="rt-navbar-links">
+    <div class="rt-navbar-link rt-navbar-link-category">分類</div>
+    <div class="rt-navbar-link rt-navbar-link-order">訂單</div>
+    <div class="rt-navbar-link rt-navbar-link-upload"></div>
+    <div class="rt-navbar-link rt-navbar-link-upload-shadow"></div>
+    <div class="rt-navbar-link rt-navbar-link-info">通知</div>
+    <div class="rt-navbar-link rt-navbar-link-account">我的</div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'rt-navbar'
+}
+</script>
