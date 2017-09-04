93ab93ee03f2fa58293ab83c9c0d496890ecf161
Steve Yu <soundedrain@gmail.com>
2017-09-05 01:20:15 +0800

    [modify] footer restored

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,7 @@
 <template>
 <div class="rt-footer">
-  <div class="rt-footer-link rt-footer-link-category">分類</div>
-  <div class="rt-footer-link rt-footer-link-order">訂單</div>
-  <div class="rt-footer-link rt-footer-link-upload"></div>
-  <div class="rt-footer-link rt-footer-link-upload-shadow"></div>
-  <div class="rt-footer-link rt-footer-link-info">通知</div>
-  <div class="rt-footer-link rt-footer-link-account">我的</div>
-  <!-- <div class="rt-footer-title">
-    <a class="rt-footer-logo nav-brand-logo" href="#" data-type="go-to-index"></a>
+  <div class="rt-footer-title">
+    <!-- <a class="rt-footer-logo nav-brand-logo" href="#" data-type="go-to-index"></a> -->
     <span class="rt-footer-note">露天市集國際資訊股份有限公司</span>
   </div>
   <ul class="rt-footer-links">
@@ -15,7 +9,7 @@
     <li><a class="rt-footer-link" id="footer_prive_declaration" href="http://www.ruten.com.tw/system/server_center.htm?00060002" data-type="footer-link">隱私權聲明</a></li>
     <li><a class="rt-footer-link" id="footer_member_contract" href="http://www.ruten.com.tw/system/server_center.htm?00060001" data-type="footer-link">會員合約</a></li>
       <li><a class="rt-footer-link" id="footer_go_pc" href="http://www.ruten.com.tw/system/chgview.php?urlto=%2Fmybid%2Fmy_account.php" data-type="footer-link">電腦版</a></li>
-  </ul> -->
+  </ul>
 </div>
 </template>
 
